a2a8100202eb609cb45be0c611fbd099d3d57bf4
Zach <zgleason37@gmail.com>
2022-04-21 12:03:30 -0700

    add

--- Server/Server.js
+++ Server/Server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const cors = require('cors')    
 const app = express();
-app.use(cors())                
+app.use(cors())          
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require('./config/mongoose.config');
 const allMyProductRoutes = require('./routes/product.routes')
 allMyProductRoutes(app);
 
-app.listen(3000, () => {
-    console.log("Listening at Port 3000")
+app.listen(5000, () => {
+    console.log("Listening at Port 5000")
 })
